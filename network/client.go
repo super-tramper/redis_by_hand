fddeb7dc70a84bceb9d78e3c4993ca09e1a0de7d
staphaness@163.com <staphaness@163.com>
2023-03-29 20:38:21 +0800

    TCP 服务端和客户端

---
+++ network/client.go
@@ -0,0 +1,56 @@
+package network
+
+import (
+	"bufio"
+	"fmt"
+	log "github.com/sirupsen/logrus"
+	"net"
+	"os"
+	"strings"
+)
+
+func RunClient() {
+	conn, err := net.Dial("tcp", "127.0.0.1:1234")
+
+	if err != nil {
+		log.Errorf("create client connction error: %v", err)
+		return
+	}
+	defer conn.Close()
+
+	for {
+		if n, err := sendToServer(conn); err != nil || n == 0 {
+			log.Errorf("send to server error: %v", err)
+			continue
+		}
+		readFromServer(conn)
+	}
+}
+
+func sendToServer(conn net.Conn) (int, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	inputContent := strings.Trim(input, "\r\n")
+	fmt.Printf("input content: %v\n", inputContent)
+
+	if strings.ToUpper(inputContent) == "Q" {
+		os.Exit(0)
+	}
+
+	n, err := conn.Write([]byte(inputContent))
+	if err != nil {
+		log.Errorf("client send message failed: %v", err)
+		return 0, err
+	}
+	return n, nil
+}
+
+func readFromServer(conn net.Conn) {
+	fmt.Println("in read")
+	buf := [512]byte{}
+	n, err := conn.Read(buf[:])
+	if err != nil {
+		log.Errorf("cilent read message failed: %v", err)
+	}
+	fmt.Println(string(buf[:n]))
+}
---
+++ network/server.go
@@ -0,0 +1,66 @@
+package network
+
+import (
+	"bufio"
+	log "github.com/sirupsen/logrus"
+	"net"
+)
+
+func RunServer() {
+	addr := ":1234"
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Errorf("runserver error: %v", err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Infof("accept error: %v", err)
+			continue
+		}
+		defer conn.Close()
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	// 处理连接产生panic时从错误恢复，包装server能正常运行
+	defer func() {
+		if err := recover(); err != nil {
+			log.Errorf("panic: %v", err)
+		}
+	}()
+
+	for {
+		received, err := readFromClient(conn)
+		if err != nil {
+			log.Errorf("read from client error: %v", err)
+			panic(err)
+		}
+		msg := "echo: " + received
+		if err := sendToClient(msg, conn); err != nil {
+			log.Errorf("send to client error: %v", err)
+			panic(err)
+		}
+	}
+}
+
+func readFromClient(conn net.Conn) (string, error) {
+
+	reader := bufio.NewReader(conn)
+	var buf [128]byte
+	n, err := reader.Read(buf[:])
+	if err != nil {
+		log.Errorf("read from client failed: %v", err)
+		return "Error", err
+	}
+	recvStr := string(buf[:n])
+	return recvStr, nil
+}
+
+func sendToClient(msg string, conn net.Conn) error {
+	if _, err := conn.Write([]byte(msg)); err != nil {
+		return err
+	}
+	return nil
+}
